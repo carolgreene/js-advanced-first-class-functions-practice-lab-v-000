61219cc883e5dd84e34261a7dd17382b20c993ba
Carol Greene <carolgreene3@gmail.com>
2018-10-26 21:35:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,4 +29,6 @@ const totalRevenue = function(drivers) {
     return currentDriver.revenue + total
   }, 0)
 }
-
+const averageRevenue = function(drivers) {
+  return totalRevenue(drivers) / drivers.length
+}
