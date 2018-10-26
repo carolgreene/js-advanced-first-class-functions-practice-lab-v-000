d6ba0b161f1bb5170fdc4ab6d947ffa7be7da5e9
Carol Greene <carolgreene3@gmail.com>
2018-10-26 21:11:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,3 @@ const logDriversByHometown = function(drivers, location) {
     console.log(driver.name)
   })
 }
-
-const driversByRevenue = function(drivers) {
-  return drivers.slice().sort(function(driver1, driver2) {
-    return driver1.revenue - driver2.revenue
-  })
-}
